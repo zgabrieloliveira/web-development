c38e02a3052b2de6599c912737f5cbd58435eabc
zgabrieloliveira <zgabrieloliveir4@gmail.com>
2024-09-19 21:57:03 -0300

    fix: ex4 do trabalho 7

--- trabalho7/ex4/script.js
+++ trabalho7/ex4/script.js
@@ -1,17 +1,13 @@
 document.forms.signup.onsubmit = validate;
-
 function validate(e) {
   let form = e.target;
   let valid = true;
-
   const errorUser = form.user.nextElementSibling;
   const errorPassword = form.password.nextElementSibling;
   const errorEmail = form.email.nextElementSibling;
-
   errorUser.textContent = "";
   errorPassword.textContent = "";
   errorEmail.textContent = "";
-
   if (form.user.value === "") {
     errorUser.textContent = "Campo obrigatório";
     valid = false;
@@ -24,9 +20,7 @@ function validate(e) {
     errorEmail.textContent = "Campo obrigatório";
     valid = false;
   }
-
   if (!valid) {
     e.preventDefault();
   }
 }
-
